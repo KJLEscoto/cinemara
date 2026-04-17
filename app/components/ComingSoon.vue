ef50a1ad606e6e7b960dc866d7723d4095b85ea7
Kent Joemar Escoto <kentescoto24@gmail.com>
2026-04-17 23:47:00 +0800

    changed coming soon ui

--- app/components/ComingSoon.vue
+++ app/components/ComingSoon.vue
@@ -24,51 +24,52 @@ function notified() {
 </script>
 
 <template>
-  <main class="min-h-screen px-4 py-20 w-full flex items-center justify-center relative overflow-hidden">
+  <main class="min-h-screen md:px-20 px-4 py-20 w-full flex flex-col items-start justify-between gap-10 relative overflow-hidden">
 
     <!-- blurs -->
-    <div class="bg-primary md:size-52 size-32 rounded-full absolute -top-20 blur-[120px] left-40"></div>
-    <div class="bg-primary md:size-52 size-32 rounded-full absolute -bottom-20 blur-[120px] right-0"></div>
+    <div class="bg-primary md:size-52 size-32 rounded-full absolute top-60 blur-[100px] -left-40"></div>
+    <!-- <div class="bg-primary md:size-52 size-32 rounded-full absolute -bottom-20 blur-[120px] right-0"></div> -->
 
-    <div class="space-y-10 grid place-items-center max-w-4xl w-full z-10">
+    <div class="space-y-10 grid md:place-items-start place-items-center w-full h-full justify-between z-10">
       <section class="flex items-center md:gap-3 gap-1">
         <img class="md:size-12 size-8" src="/images/Icon.png" alt="cinemara icon">
         <p class="font-semibold md:text-3xl text-2xl">Cinemara</p>
       </section>
 
-      <section class="font-black uppercase text-center md:text-5xl text-3xl md:space-y-1">
-        <h1>YOUR NEXT OBSESSION</h1>
-        <h1 class="text-primary">IS ALMOST HERE.</h1>
+      <section class="font-black uppercase md:text-start text-center md:text-8xl text-5xl md:space-y-1">
+        <h1>YOUR NEXT <br /> OBSESSION</h1>
+        <h1 class="text-primary">IS ALMOST <br /> HERE.</h1>
       </section>
 
-      <section class="text-center md:text-lg text-base md:-mt-4 -mt-8">
-        <p class="text-muted">Cinemara is where you explore films, browse overviews, and watch trailers — all before
-          deciding what's next. Drop your email and we'll tell you the moment we go live.</p>
+      <section class="text-center md:text-xl text-base md:-mt-4 -mt-8">
+        <p class="text-muted">Explore films, browse overviews, and watch trailers — all before
+          deciding what's next.</p>
       </section>
 
-      <section class="flex items-center gap-5 w-full">
+      <!-- <section class="flex items-center gap-5 w-full">
         <hr class="w-full border-white/5">
         <div class="size-3 ring-8 ring-primary/10 bg-primary rounded-full shrink-0"></div>
         <hr class="w-full border-white/5">
-      </section>
+      </section> -->
 
-      <section class="space-y-2 max-w-xl w-full">
-        <p class="md:text-base text-sm">Get notified when we launch</p>
-        <form @submit.prevent="notified" class="flex md:flex-row flex-col items-start gap-2 w-full">
-          <div class="space-y-2 w-full">
-            <input v-model="email" class="bg-white placeholder:text-black/40 md:p-4 p-3 text-black rounded-lg w-full"
-              :class="{ 'ring-2 ring-primary': error }" type="email" name="email_address" id="email_address"
-              placeholder="Enter your email address">
-            <p v-if="error" class="text-primary text-sm">{{ error }}</p>
-          </div>
-          <button type="submit"
-            class="bg-primary md:py-4 py-3 px-6 rounded-lg md:w-fit w-full cursor-pointer flex items-center justify-center gap-2 text-nowrap">
-            <BellRing class="pointer-events-none size-5" />
-            Notify Me
-          </button>
-        </form>
-      </section>
     </div>
+
+    <section class="space-y-2 max-w-xl w-full">
+      <p class="md:text-base text-sm">Get notified when we launch!</p>
+      <form @submit.prevent="notified" class="flex md:flex-row flex-col items-start gap-2 w-full">
+        <div class="space-y-2 w-full">
+          <input required v-model="email" class="bg-white placeholder:text-black/40 md:p-4 p-3 text-black rounded-lg w-full"
+            :class="{ 'ring-2 ring-primary': error }" type="email" name="email_address" id="email_address"
+            placeholder="Enter your email address">
+          <p v-if="error" class="text-primary text-sm">{{ error }}</p>
+        </div>
+        <button type="submit"
+          class="bg-primary md:py-4 py-3 px-6 rounded-lg md:w-fit w-full cursor-pointer flex items-center justify-center gap-2 text-nowrap">
+          <BellRing class="pointer-events-none size-5" />
+          Notify Me
+        </button>
+      </form>
+    </section>
 
   </main>
 </template>
